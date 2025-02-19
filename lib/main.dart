1c03aa37f9403a75492f42ee85d1b9f76be460b9
rudsonalves <rudsonalves67@gmail.com>
2025-02-19 12:59:13 -0300

    Ajustes...

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:route_aware/data/app_counter.dart';
 import 'package:route_aware/my_app.dart';
 
 void main() async {
   // Initialize Flutter binding
   WidgetsFlutterBinding.ensureInitialized();
-
-  final app = AppCounter.instance;
-  await app.initialize();
 
   runApp(const MyApp());
 }
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:route_aware/data/app_counter.dart';
 import 'package:routefly/routefly.dart';
 
 import 'my_app.route.dart';
@@ -6,8 +7,22 @@ import 'my_app.route.dart';
 part 'my_app.g.dart';
 
 @Main('lib/ui')
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final app = AppCounter.instance;
+
+  @override
+  void initState() {
+    super.initState();
+
+    app.initialize();
+  }
 
   @override
   Widget build(BuildContext context) {
